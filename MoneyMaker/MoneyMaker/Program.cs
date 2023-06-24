d5fdb6833a41341fe1add3f965346a4fba697ea4
Nelson Villegas <98365702+Nutella4Mandela@users.noreply.github.com>
2023-06-23 22:45:23 -0400

    Add files via upload

---
+++ MoneyMaker/MoneyMaker/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MoneyMaker
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Money Maker!");
+
+            Console.WriteLine("Enter an amount to convert to coins:");
+            string coinString = Console.ReadLine();
+
+            double coinDouble = Convert.ToDouble(coinString);
+
+            double goldValue = 10;
+            double silverValue = 5;
+
+            double goldCoins = Math.Floor(coinDouble / goldValue);
+            double bronzeCoins = coinDouble % goldValue;
+
+            double silverCoins = Math.Floor(bronzeCoins / silverValue);
+            bronzeCoins = coinDouble % silverValue;
+
+            Console.WriteLine($"{coinDouble} cents is equal to...");
+            Console.WriteLine($"Gold coins: {goldCoins}");
+            Console.WriteLine($"Silver coins: {silverCoins}");
+            Console.WriteLine($"Bronze coins: {bronzeCoins}");
+
+
+        }
+    }
+}
